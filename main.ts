33065f6b897e6e5edf157d5cb5f9d00d1a45b634
DavidUdic-P1A <115144133+DavidUdic-P1A@users.noreply.github.com>
2022-12-16 19:41:21 +0100

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,48 @@
+const min = 500;
+const max = 5000;  
+
+
+basic.forever(function () {
+    
+    let interval = randint(min, max);
+
+    music.playTone(Note.C, interval);
+
+    while (true) {
+        if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
+            break
+        }
+    }
+
+    
+    const start = control.millis();
+
+    while (true) {
+        if (!(input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B))) {
+            break
+        }
+    }
+
+   
+    const end = control.millis()
+    const elapsed = end - start;
+
+    const diff = Math.abs(interval - elapsed);
+
+    if (diff < 500) {
+        basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    } else {
+        basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #`)
+    }
+})
